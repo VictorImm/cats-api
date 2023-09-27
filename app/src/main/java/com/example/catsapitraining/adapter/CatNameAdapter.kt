c36520089bcccc38dbf359e8daf8d1f2f00c6cb9
VictorImm <victorimmanuelss@gmail.com>
2023-09-27 23:51:59 +0700

    [UPDATE] add click listener to holder

--- app/src/main/java/com/example/catsapitraining/adapter/CatNameAdapter.kt
+++ app/src/main/java/com/example/catsapitraining/adapter/CatNameAdapter.kt
@@ -8,15 +8,18 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.catsapitraining.R
 import com.example.catsapitraining.data.Cat
 import com.example.catsapitraining.databinding.RowCatBinding
+import com.example.catsapitraining.ui.catview.CatListFragmentDirections
 
 class CatNameAdapter(
-    private val context: Context
+    private val context: Context,
+    private val navController: NavController
 ): RecyclerView.Adapter<CatNameAdapter.CatViewHolder>() {
 
     private val list = ArrayList<Cat>()
@@ -49,14 +52,19 @@ class CatNameAdapter(
     override fun getItemCount(): Int = list.size
 
     override fun onBindViewHolder(holder: CatNameAdapter.CatViewHolder, position: Int) {
-        val (name, image_link) = list[position]
+        val cat = list[position]
 
         Glide.with(context)
-            .load(image_link)
+            .load(cat.image_link)
             .transition(DrawableTransitionOptions.withCrossFade())
             .centerCrop()
             .into(holder.tvPicture)
 
-        holder.tvName.text = name
+        holder.tvName.text = cat.name
+
+        holder.itemView.setOnClickListener {
+            val action = CatListFragmentDirections.actionCatListFragmentToCatDetailFragment(cat)
+            navController.navigate(action)
+        }
     }
 }
